248f492c258149cb7b333a68c10e776d01c58277
yoho <yoho@yohodeMacBook-Pro.local>
2022-07-27 15:46:12 +0800

    s

--- dist/npm.export.js
+++ dist/npm.export.js
@@ -1,5 +1,1 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./bttc.node.min.js')
-} else {
-  module.exports = require('./bttc.node.js')
-}
+module.exports = require('./bttc.node.min.js')
